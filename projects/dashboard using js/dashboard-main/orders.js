f4c9ddf0382abd562d4e20b87c11362639c8f17a
NaumanSaleem22 <noumansaleem1999@gmail.com>
2022-08-25 14:45:24 +0500

    first commit

---
+++ projects/dashboard using js/dashboard-main/orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85521",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Order12",
+    productNumber: "33243",
+    paymentStatus: "Refunded",
+    shipping: "Pending",
+  },
+  {
+    productName: "Order4",
+    productNumber: "15521",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Car",
+    productNumber: "55442",
+    paymentStatus: "Due",
+    shipping: "Declined",
+  },
+  {
+    productName: "Jahaz",
+    productNumber: "35521",
+    paymentStatus: "Refunded",
+    shipping: "Delivered",
+  },
+];
